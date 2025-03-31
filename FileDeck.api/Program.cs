a879ce5c01ab2c0432cccdf09c653d0c33a6f6bd
LudwigStenberg <lludwigstenberg@gmail.com>
2025-03-31 18:58:58 +0200

    Add docker-compose to project

--- FileDeck.api/Program.cs
+++ FileDeck.api/Program.cs
@@ -43,5 +43,8 @@ public class Program
         app.UseHttpsRedirection();
         app.UseAuthorization();
         app.MapControllers();
+
+        // Start the application
+        app.Run();
     }
 }

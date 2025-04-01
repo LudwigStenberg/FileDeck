7d292cbb6e97b9b4a9da84330fb53f8192f5a747
LudwigStenberg <lludwigstenberg@gmail.com>
2025-04-01 09:11:35 +0200

    Build FolderEntity.cs

--- FileDeck.api/Models/FolderEntity.cs
+++ FileDeck.api/Models/FolderEntity.cs
@@ -4,20 +4,21 @@ using System.Collections.Generic;
 public class FolderEntity
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public ICollection<FileEntity> Files { get; set; }
-    public DateTime CreatedDate { get; set; }
+    public required string Name { get; set; }
+    public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+    public DateTime LastModifiedDate { get; set; } = DateTime.UtcNow;
+    public bool IsDeleted { get; set; } = false; // Soft Delete
 
 
-    // Relationship to other Folders
-    public int? ParentFolderId { get; set; }
-    public FolderEntity ParentFolder { get; set; }
-    public ICollection<FolderEntity> SubFolders { get; set; }
+    // Relationship to other Folders (self-referencing)
+    public int? ParentFolderId { get; set; } // FK, nullable for root folders 
+    public FolderEntity? ParentFolder { get; set; } // NavProp to parent
+    public ICollection<FolderEntity> SubFolders { get; set; } = new List<FolderEntity>(); // NavProp to children
 
     // Relationship to Files (One-to-Many)
-    public ICollection<FileEntity> Files { get; set; }
+    public ICollection<FileEntity> Files { get; set; } = new List<FileEntity>();
 
     // Relationship to User (many-to-one, many folders - one user)
-    public string UserId { get; set; }
-    public ApplicationUser User { get; set; }
+    // public string UserId { get; set; } // FK
+    // public ApplicationUser User { get; set; } // NavProp
 }

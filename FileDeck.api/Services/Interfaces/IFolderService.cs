bd5c494f12554bd19f189e2e17c697341ecfcb4c
LudwigStenberg <lludwigstenberg@gmail.com>
2025-04-03 16:01:40 +0200

    Define GetFolderByIdAsaync in interface

--- FileDeck.api/Services/Interfaces/IFolderService.cs
+++ FileDeck.api/Services/Interfaces/IFolderService.cs
@@ -6,4 +6,6 @@ namespace FileDeck.api.Services.Interfaces;
 public interface IFolderService
 {
     Task<FolderResponseDto> CreateFolderAsync(CreateFolderDto folderDto, string userId);
+    Task<FolderResponseDto?> GetFolderByIdAsync(int folderId);
+
 }

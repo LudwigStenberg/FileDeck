2b980d566b65537ccf923838b1b923af5c20ca02
LudwigStenberg <lludwigstenberg@gmail.com>
2025-04-06 09:56:08 +0200

    Create HttpGet GetUserById

--- FileDeck.api/Controllers/AuthController.cs
+++ FileDeck.api/Controllers/AuthController.cs
@@ -1,12 +1,15 @@
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using FileDeck.api.DTOs.Auth;
 using FileDeck.api.Models;
 using FileDeck.api.Services;
 using FileDeck.api.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Npgsql.Replication;
 
 namespace FileDeck.api.Controllers;
 
@@ -16,33 +19,54 @@ public class AuthController : ControllerBase
 {
     private readonly ITokenService tokenService;
     private readonly IAuthService authService;
+    private readonly UserManager<UserEntity> userManager;
 
-    public AuthController(ITokenService tokenService, IAuthService authService)
+    public AuthController(ITokenService tokenService, IAuthService authService, UserManager<UserEntity> userManager)
     {
         this.tokenService = tokenService;
         this.authService = authService;
+        this.userManager = userManager;
     }
 
     [HttpPost("register")]
     public async Task<IActionResult> RegisterUserAsync(RegisterRequestDto registerDto)
     {
-        if (registerDto == null)
+        // Based on the attributes of RegisterRequestDto
+        if (!ModelState.IsValid)
         {
-            return ...?
+            return BadRequest(ModelState);
         }
 
-        var registerResponseDto = await authService.RegisterUserAsync(registerDto);
+        var result = await authService.RegisterUserAsync(registerDto);
 
 
-        if (registerResponseDto.Succeeded)
+        if (result.Succeeded)
         {
             return CreatedAtAction(nameof(?))
         }
 
-        return registerResponseDto.Errors.ToList(e => e.Description.ToList());
-
+        return BadRequest(result);
     }
 
+    [HttpGet("{id}")]
+    [Authorize]
+    public async Task<IActionResult> GetUserById(string id)
+    {
+        // Get the current user from the token
+        var currentUserId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
 
+        if (id != currentUserId)
+        {
+            return Forbid();
+        }
 
+        var user = await userManager.FindByIdAsync(id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(new { user.Id, user.Email });
+    }
 }

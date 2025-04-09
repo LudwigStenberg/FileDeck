18870dddcdbb2f4617581d5e8cb9f0f9d98052bf
LudwigStenberg <lludwigstenberg@gmail.com>
2025-04-09 09:02:01 +0200

    Implement FileMetadataDto

--- FileDeck.api/DTOs/File/FileMetadataDto.cs
+++ FileDeck.api/DTOs/File/FileMetadataDto.cs
@@ -1,3 +1,11 @@
+using System;
+
 public class FileMetadataDto
 {
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string ContentType { get; set; } = string.Empty;
+    public long Size { get; set; }
+    public DateTime UploadDate { get; set; }
+    public DateTime LastModifiedDate { get; set; }
 }

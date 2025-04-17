33e0dbf27ede4feb4943b3cd92a04d1c2da6b578
LudwigStenberg <contact@ludwigtenberg.com>
2025-04-17 12:53:34 +0200

    Setup pre-configured axios instance

--- filedeck.client/src/services/api.ts
+++ filedeck.client/src/services/api.ts
@@ -8,3 +8,21 @@ const api = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers = config.headers || {};
+      config.headers.Authorization = "Bearer ${token}";
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;

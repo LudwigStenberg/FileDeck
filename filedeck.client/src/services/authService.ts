38e6699931060d35fa1ee422e611d74370969f10
LudwigStenberg <contact@ludwigtenberg.com>
2025-04-17 15:51:15 +0200

    Create logout function

--- filedeck.client/src/services/authService.ts
+++ filedeck.client/src/services/authService.ts
@@ -16,5 +16,23 @@ export const register = async (
   );
   return response.data;
 };
+
 // Login
+export const login = async (
+  loginData: LoginRequestDto
+): Promise<LoginResponseDto> => {
+  const response = await api.post<LoginResponseDto>("auth/login", loginData);
+
+  if (response.data.succeeded && response.data.token) {
+    localStorage.setItem("token", response.data.token);
+    localStorage.setItem("userId", response.data.userId);
+  }
+
+  return response.data;
+};
+
 // Logout
+export const logout = (): void => {
+  localStorage.removeItem("token");
+  localStorage.removeItem("userId");
+};

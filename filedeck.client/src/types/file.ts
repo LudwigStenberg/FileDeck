399d94b1670908e54c76732c12b1e9d0a02f6cfb
LudwigStenberg <contact@ludwigtenberg.com>
2025-04-17 11:25:39 +0200

    Add types/interfaces for file DTOs

---
+++ filedeck.client/src/types/file.ts
@@ -0,0 +1,23 @@
+export interface FileUpload {
+  name: string;
+  contentType: string;
+  content: Uint8Array | ArrayBuffer;
+  folderId: number;
+}
+
+export interface FileDownload {
+  id: number;
+  name: string;
+  contentType: string;
+  content: Uint8Array | ArrayBuffer;
+}
+
+export interface FileResponse {
+  id: number;
+  name: string;
+  contentType: string;
+  size: number;
+  uploadDate: string;
+  lastModifiedDate: string;
+  folderId: number;
+}

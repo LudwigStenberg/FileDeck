dce21c306e888e6b35d1a7e39251309ea307559e
LudwigStenberg <lludwigstenberg@gmail.com>
2025-04-20 16:59:04 +0200

    Create AuthProvider

--- filedeck.client/src/context/AuthContext..tsx
+++ filedeck.client/src/context/AuthContext..tsx
@@ -1,3 +1,4 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
 import { LoginRequestDto, RegisterRequestDto } from "../types";
 
 interface AuthContextType {
@@ -8,3 +9,27 @@ interface AuthContextType {
   register: (data: RegisterRequestDto) => Promise<boolean>;
   logout: () => void;
 }
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [userId, setUserId] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const storedUserId = localStorage.getItem("userId");
+
+    if (token && storedUserId) {
+      setIsAuthenticated(true);
+      setUserId(storedUserId);
+    }
+
+    setIsLoading(false);
+  }, []);
+};

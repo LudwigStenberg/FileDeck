ef907628d75b4fac0e49d5f9ff510b698dbde0de
LudwigStenberg <lludwigstenberg@gmail.com>
2025-04-20 14:47:22 +0200

    Add DashboardPage

--- filedeck.client/src/main.tsx
+++ filedeck.client/src/main.tsx
@@ -5,15 +5,26 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
 import HomePage from "./pages/HomePage.tsx";
+import RegisterPage from "./pages/RegisterPage.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
+import DashboardPage from "./pages/DashboardPage.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
   },
-
   {
     path: "/register",
+    element: <RegisterPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/dashboard",
+    element: <DashboardPage />,
   },
 ]);
 

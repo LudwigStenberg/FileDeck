9e3f05a99767aeff8dd1775f1a07a02c748e1772
LudwigStenberg <lludwigstenberg@gmail.com>
2025-04-20 15:14:30 +0200

    Add links for routing on Homepage

--- filedeck.client/src/pages/HomePage.tsx
+++ filedeck.client/src/pages/HomePage.tsx
@@ -1,5 +1,20 @@
+import { Link } from "react-router";
 import "../index.css";
 
 export default function HomePage() {
-  return <h1>Home Page</h1>;
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }

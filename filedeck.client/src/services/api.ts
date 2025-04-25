76176206c858c76d8d12de12d3ea5a41ea0bd9c3
LudwigStenberg <lludwigstenberg@gmail.com>
2025-04-25 09:19:13 +0200

    replace  with backticks

--- filedeck.client/src/services/api.ts
+++ filedeck.client/src/services/api.ts
@@ -15,7 +15,7 @@ api.interceptors.request.use(
 
     if (token) {
       config.headers = config.headers || {};
-      config.headers.Authorization = "Bearer ${token}";
+      config.headers.Authorization = `Bearer ${token}`;
     }
 
     return config;

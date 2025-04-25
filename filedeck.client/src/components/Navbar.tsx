fd2d910729783379a76d8c11b1baf67b89ef3440
LudwigStenberg <lludwigstenberg@gmail.com>
2025-04-25 18:10:27 +0200

    Add logout functionality to navbar

--- filedeck.client/src/components/Navbar.tsx
+++ filedeck.client/src/components/Navbar.tsx
@@ -1,17 +1,36 @@
+import { Link, useNavigate } from "react-router";
 import { useAuth } from "../context/AuthContext";
-
 import logo from "../assets/logo.png";
-import { Link } from "react-router";
+import { logout } from "../services/authService";
+
 
 export const Navbar = () => {
+		const  { userId, isAuthenticated, logout } = useAuth();
+		const navigate = useNavigate();
 
     return (
         <div className="navbar-container">
         	<img src={logo} alt="FileDeck Logo" className="navbar-logo"/>
           <ul className="navbar-list">
-            <li><Link to="/dashboard">Dashboard</Link></li>
-						<li><Link to="/profile">Profile</Link></li>
+            <li><Link to="/dashboard" className="navbar-link">Dashboard</Link></li>
+						<li><Link to="/profile" className="navbar-link">Profile</Link></li>
           </ul>
+
+					<div className="user-controls">
+						{isAuthenticated && (
+							<>
+							<button
+								className="logout-button"
+								onClick={() => {
+									logout();
+									navigate("/");
+
+								}}
+								>Logout</button>
+							</>
+						)}
+
+					</div>
         </div>
     );
 }

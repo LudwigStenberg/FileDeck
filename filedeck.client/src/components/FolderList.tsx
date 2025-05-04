9518aeb804bbf7af407d64452f25593382735e83
LudwigStenberg <lludwigstenberg@gmail.com>
2025-05-04 12:15:49 +0200

    Create handleFolderClick and handleGoUp

--- filedeck.client/src/components/FolderList.tsx
+++ filedeck.client/src/components/FolderList.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { FolderResponse } from "../types";
-import * as folderService from "../services/folderService";
 import { FaRegFolder } from "react-icons/fa";
+import { IoArrowBack } from "react-icons/io5";
 import "../styles/folder.css";
 
 interface FolderListProps {
@@ -18,9 +18,20 @@ export const FolderList = ({
   const foldersToDisplay = folders.filter(
     (folder) =>
       currentFolderId === null
-        ? folder.parentFolderId === null // Root folders
-        : folder.parentFolderId === currentFolderId // Subfolders
+        ? folder.parentFolderId === null // Show root folders when at root
+        : folder.parentFolderId === currentFolderId // Show subfolders when in a folder
   );
+
+  const handleFolderClick = (folderId: number) => {
+    setCurrentFolderId(folderId);
+  };
+
+  const handleGoUp = () => {
+    if (currentFolderId === null) return;
+
+    const currentFolder = folders.find((f) => f.id === currentFolderId);
+    setCurrentFolderId(currentFolder?.parentFolderId ?? null);
+  };
 
   if (folders.length === 0) {
     return <div>No folders found. Create a folder to get started.</div>;
@@ -29,14 +40,25 @@ export const FolderList = ({
   return (
     <div className="folder-list">
       <h2>Folders</h2>
+
+      <div className="navigation-container">
+        <IoArrowBack className="go-up-icon" size={25} />
+        <span className="navigation-path">Path</span>
+      </div>
+
       <div className="folder-list-header">
+        <span></span>
         <span className="folder-name-header">Name</span>
         <span className="folder-created-header">Created</span>
       </div>
       <div className="folder-items">
         <FaRegFolder className="folder-icon" size={20} />
-        {folders.map((folder) => (
-          <li key={folder.id} className="folder-item">
+        {foldersToDisplay.map((folder) => (
+          <li
+            key={folder.id}
+            className="folder-item"
+            onClick={() => handleFolderClick(folder.id)}
+          >
             <span className="folder-name">{folder.name}</span>
             <span className="folder-created">
               {new Date(folder.createdDate).toLocaleDateString()}

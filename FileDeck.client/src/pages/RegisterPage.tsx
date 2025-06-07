0c487283709c194b0496adc7acfe32dd1bf8a343
LudwigStenberg <lludwigstenberg@gmail.com>
2025-06-07 19:19:21 +0200

    remove: ProfilePage.tsx and delete unused import

--- FileDeck.client/src/pages/RegisterPage.tsx
+++ FileDeck.client/src/pages/RegisterPage.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 import { Register } from "../components/auth/Register";
 import { useAuth } from "../context/AuthContext";
 
